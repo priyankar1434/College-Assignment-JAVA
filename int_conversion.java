b938adec4fb2c835ef67eb1521a737f8d2147347
Priyankar Shukla <165669263+priyankar1434@users.noreply.github.com>
2024-10-28 17:40:45 +0530

    Create int_conversion.java

---
+++ int_conversion.java
@@ -0,0 +1,11 @@
+package Assignmet;
+import java.util.Scanner;
+
+public class int_con {
+    public static void main(String[] args){
+        float floatprice = 79.95f;
+        double intprice = (double) floatprice;
+        System.out.println("Previous Output: " + floatprice + " New Output: " + intprice);
+
+    }
+}
